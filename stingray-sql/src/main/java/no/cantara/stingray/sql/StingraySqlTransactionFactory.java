d5ba39648d6a7cb44218853fecef8cb55c33a700
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2022-03-01 16:35:02 +0100

    Added runInTransaction convenience api

--- stingray-sql/src/main/java/no/cantara/stingray/sql/StingraySqlTransactionFactory.java
+++ stingray-sql/src/main/java/no/cantara/stingray/sql/StingraySqlTransactionFactory.java
@@ -1,6 +1,15 @@
 package no.cantara.stingray.sql;
 
+import java.util.function.Consumer;
+
 public interface StingraySqlTransactionFactory {
 
     StingraySqlTransaction createTransaction() throws StingraySqlException;
+
+    default void runInTransaction(Consumer<StingraySqlTransaction> work) throws StingraySqlException {
+        try (StingraySqlTransaction transaction = createTransaction()) {
+            work.accept(transaction);
+            transaction.commit();
+        }
+    }
 }
