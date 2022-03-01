14cf13fc916ebfb08514203235ca4fa55748231c
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2022-03-01 17:08:47 +0100

    Added overloaded run-in-transaction with return value.

--- stingray-sql/src/main/java/no/cantara/stingray/sql/StingraySqlTransactionFactory.java
+++ stingray-sql/src/main/java/no/cantara/stingray/sql/StingraySqlTransactionFactory.java
@@ -1,6 +1,7 @@
 package no.cantara.stingray.sql;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public interface StingraySqlTransactionFactory {
 
@@ -10,6 +11,14 @@ public interface StingraySqlTransactionFactory {
         try (StingraySqlTransaction transaction = createTransaction()) {
             work.accept(transaction);
             transaction.commit();
+        }
+    }
+
+    default <R> R runInTransaction(Function<StingraySqlTransaction, R> work) throws StingraySqlException {
+        try (StingraySqlTransaction transaction = createTransaction()) {
+            R result = work.apply(transaction);
+            transaction.commit();
+            return result;
         }
     }
 }
